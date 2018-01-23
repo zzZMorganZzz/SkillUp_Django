3509ebba7a0b63ece4ec0f168f4bdd3bc01e4ab7
Unknown <mikhaylov-kg@mail.ru>
2018-01-23 11:27:54 +1000

    Add SuperUser Add Models GUI

--- TwitterProject/admin.py
+++ TwitterProject/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Tag, Account, Post, Likes
 
 # Register your models here.
+admin.site.register(Tag)
+admin.site.register(Account)
+admin.site.register(Post)
+admin.site.register(Likes)
