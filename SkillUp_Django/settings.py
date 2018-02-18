bcb89fade4dd6b00c5cad55d9516ad1dd296fcba
Unknown <mikhaylov-kg@mail.ru>
2018-02-18 19:53:04 +1000

    Потер старый проект

--- SkillUp_Django/settings.py
+++ SkillUp_Django/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'TwitterProject',
+    #'TwitterProject',
+    'EventCityProject',
 ]
 
 MIDDLEWARE = [
